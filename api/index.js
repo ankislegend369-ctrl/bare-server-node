6898565a042c6655e04cc55bd4bf26684a0ed5cb
ankislegend369-ctrl <ankislegend369@gmail.com>
2026-05-19 18:40:00 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
+// api/index.js
+
+const { createBareServer } = require('@tomphttp/bare-server-node');
+const http = require('http');
+
+// Create Bare server with the /bare/ path
+const bare = createBareServer('/bare/');
+
+// Export a handler for Vercel
+module.exports = (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+};
 
